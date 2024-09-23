9786164713cf019d1f077307546ce5cd4919413d
kl369 <152214213+kl369@users.noreply.github.com>
2024-09-23 16:09:14 +0300

    Delete kubeadm-clusters directory

--- kubeadm-clusters/virtualbox/mac/mac-bridge.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-iface=$(netstat -rn -f inet | grep '^default' | sort | head -1 | awk '{print $4}')
-interface=$(VBoxManage list bridgedifs | awk '/^Name:/ { print }' | sed -e 's/^Name:[ ]*\(.*\)/\1/' | grep $iface)
-echo -n $interface
